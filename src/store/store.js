b8adb0e526c38927fd407a9d251c8cf7f41840d5
Moses Kamara <kamara.moses@yahoo.com>
2021-02-04 22:06:01 -0800

    auth for authReducers is declared

--- src/store/store.js
+++ src/store/store.js
@@ -6,6 +6,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     combineReducers({
-        
-    })
+        auth: authReducer
+    }),
 )
