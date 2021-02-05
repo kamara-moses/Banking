89fc054d0a086453fcb1f7d6e98ea1ca6ddb2d6b
Moses Kamara <kamara.moses@yahoo.com>
2021-02-04 21:51:45 -0800

    created switch cases in the authReducer in the auth.js file

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,5 +1,8 @@
 import { SIGN_IN, SIGN_OUT } from "../utils/constants";
 
-const authReducer = (state = {}, actions) => {
-    
+const authReducer = (state = {}, action) => {
+    switch (action.type) {
+        case SIGN_IN:
+            return action.user;
+    }
 }
