a39eec8183a227b70096e9eeb882cfbe3921c3d5
Moses Kamara <kamara.moses@yahoo.com>
2021-02-04 23:21:28 -0800

    imported store from store into index.js file

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from "./router/AppRouter";
 import { Provider } from "react-redux";
+import store from "./store/store";
 
