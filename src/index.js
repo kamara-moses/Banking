ccbdcb937e9f954eef5e7dcb5c7ebeb967518956
Moses Kamara <kamara.moses@yahoo.com>
2021-02-04 23:25:34 -0800

    started the render method using ReactDom

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/main.scss";
+import reactDom from 'react-dom';
 
 
 const rootElement = document.getElementById("root");
+
+reactDom.render(
+  
+)
