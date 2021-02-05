1b03caf71672c353c1f00dd8dd23d40a547cbfb2
Moses Kamara <kamara.moses@yahoo.com>
2021-02-04 21:54:09 -0800

    created the switch case for sign out in auth.js'

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -4,5 +4,7 @@ const authReducer = (state = {}, action) => {
     switch (action.type) {
         case SIGN_IN:
             return action.user;
+        case SIGN_OUT:
+            return {};
     }
 }
