d55da7e6f285a34f8d2399cb1a2e5b768728991e
Moses Kamara <kamara.moses@yahoo.com>
2021-02-04 22:14:25 -0800

    started the AppRouter functionin the AppRouter.js

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,3 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Login from "../components/Login";
+
+const AppRouter = () => {
+    
+}
