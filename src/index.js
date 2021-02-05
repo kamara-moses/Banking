4d3815684ca91552f666aafa862ab5e9fd99645f
Moses Kamara <kamara.moses@yahoo.com>
2021-02-04 23:23:42 -0800

    imported main.scss from css into index.js file

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import AppRouter from "./router/AppRouter";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import "./css/main.scss";
