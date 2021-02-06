e227cdda3452f00323a8dbcdf170ff3b6dedb05e
Moses Kamara <kamara.moses@yahoo.com>
2021-02-06 14:47:12 -0800

    completed the render function of the handleInputChange

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { divide } from "lodash";
 
 class Login extends React.Component {
     state = {
@@ -41,8 +42,38 @@ class Login extends React.Component {
     };
 
     render() {
-        
+        const { errorMsg } = this.state;
+        return (
+            <div className="login-page">
+                <h1>Banking</h1>
+                <div className="login-form">
+                    <Form onSubmit={this.handleLogin}>
+                        {errorMsg && errorMsg.signin_error && (
+                            <p className="errorMsg centered-message">
+                                {errorMsg.signin_error}
+                            </p>
+                        )}
+                        <Form.Group controlled="email">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control type="email" name="email" placeholder="Enter email" onChange={this.handleInputChange} />
+                        </Form.Group>
+                        <Form.Group controlled="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" name="password" palceholder="Enter password" onChange={this.handleInputChange} />
+                        </Form.Group>
+                        <div className="action-items">
+                            <Button variant="primary" type="submit">
+                                Login
+                            </Button>
+                            <Link to="/register" className="btn-secondary">
+                                Create Account
+                            </Link>
+                        </div>
+                    </Form>
+                </div>
+            </div>
+        );
     }
 }
 
-export default Login;
+export default connect()(Login);
