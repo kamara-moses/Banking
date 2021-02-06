dc78df6cf9084c60be25e335fac7382fb90b87dd
Moses Kamara <kamara.moses@yahoo.com>
2021-02-06 14:19:36 -0800

    imported link from react-router-dom

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return <div>Login Page</div>
