87a84845a53616dbc24ce34a5079296d0b9dfa30
Moses Kamara <kamara.moses@yahoo.com>
2021-02-06 14:54:54 -0800

    started the Register component

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,3 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
+class Register extends React.Component {
+    
+}
