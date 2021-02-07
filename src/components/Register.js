9821d08fd94e0225e2826213756f8b2b90934a98
Moses Kamara <kamara.moses@yahoo.com>
2021-02-06 18:13:50 -0800

    finsihed the render function inside of register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -57,5 +57,65 @@ class Register extends React.Component {
 
     render() {
         const { errorMsg, successMsg, isSubmitted } = this.state;
+        return (
+            <div classname="login-page">
+                <h2>Register User</h2>
+                <div className="login-form">
+                    <Form onSubmit={this.registerUser}>
+                        {errorMsg && errorMsg.signup_error ? (
+                            <p className="errorMsg center-message">
+                                {errorMsg.signup_error}
+                            </p>
+                        ) : (
+                            isSubmitted && (
+                                <p className="successMsg centered-message">
+                                    { successMsg }
+                                </p>
+                            )
+                        )}
+                        <Form.Group controlId="first_name">
+                            <Form.label>
+                                First name
+                            </Form.label>
+                            <Form.Control type="text" name="first_name" placeholder="Enter first name" onChange={this.handleInputChange} />
+                        </Form.Group>
+                        <Form.Group controlId="last_name">
+                            <Form.label>
+                                Last name
+                            </Form.label>
+                            <Form.Control type="text" name="last_name" placeholder="Enter email" onChange={this.handleInputChange} />
+                        </Form.Group>
+                        <Form.Group controlId="email">
+                            <Form.label>
+                                Email Address
+                            </Form.label>
+                            <Form.Control type="email" name="email" placeholder="Enter email" onChange={this.handleInputChange} /> 
+                        </Form.Group>
+                        <Form.Group controlId="password">
+                            <Form.label>
+                                Password
+                            </Form.label>
+                            <Form.Control type="password" name="password" placeholder="Enter password" onChange={this.handleInputChange} />
+                        </Form.Group>
+                        <Form.Group controlId="cpassword">
+                            <Form.label>
+                                Confirm Password
+                            </Form.label>
+                            <Form.Control type="password" name="cpassword" placeholder="Enter confirm password" onChange={this.handleInputChange} />
+                        </Form.Group>
+                        <div className="action-items">
+                            <Button variant="primary" type="submit">
+                                Register
+                            </Button>
+                            <Link to="/" className="btn btn-secondary">
+                                Login
+                            </Link>
+                        </div>
+                    </Form>
+                </div>
+            </div>
+        );
     }
 }
+
+export default connect()(Register);
