3c9ee4253b5d75e8d2475a13be1ecebb820020cf
Moses Kamara <kamara.moses@yahoo.com>
2021-02-06 18:16:02 -0800

    imported register into AppRouter

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Login from "../components/Login";
+import Register from "../components/Register"
 
 const AppRouter = () => {
     return (
@@ -9,6 +10,7 @@ const AppRouter = () => {
             <div className="container">
                 <Switch>
                     <Route path="/" component={Login} exact={true} />
+                    <Route path="/register" component={Register} />
                 </Switch>
             </div>
         </BrowserRouter>
