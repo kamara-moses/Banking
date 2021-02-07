eb936791be4cc2f064d2f77ae0dea2ce1ea711bd
Moses Kamara <kamara.moses@yahoo.com>
2021-02-06 17:37:39 -0800

    imported validateFields into RAegister.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Form, Button } from "react-bootstrap";
+import { validateFields } from "../utils/common";
 import { Link } from "react-router-dom";
 
 class Register extends React.Component {
-    
+
 }
