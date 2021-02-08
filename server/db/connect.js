8a7583df4c0e251499f7a3af15109c95b6db9980
Moses Kamara <kamara.moses@yahoo.com>
2021-02-07 20:45:48 -0800

    finished the variables for the pool

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,5 +1,9 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    
+    use: "postgres",
+    password: "root",
+    host: 'localhost',
+    port: "2345",
+    database: "bank_account"
 })
